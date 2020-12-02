0d68f8371cf063269ec62957beedcdbfaf55707d
Mathew Seng <mathewseng@gmail.com>
2020-12-02 10:50:47 -0600

    COMP: Update GitHub Actions from ITKModuleTemplate

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     keywords='ITK PCA Mesh',
     url=r'https://github.com/InsightSoftwareConsortium/ITKPrincipalComponentsAnalysis',
     install_requires=[
-        r'itk>=5.1.1'
+        r'itk>=5.2rc1'
     ]
     )
